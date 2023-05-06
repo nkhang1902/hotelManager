1e6a806fcec6796bc9c8a3bc5bd145f546b07203
leduytan2304 <74915968+leduytan2304@users.noreply.github.com>
2023-05-06 21:56:41 +0700

    Điền thông tin cá nhân
    Hoàn thành điền thông tin của Khách cá nhân

--- PTTK-GIaoDien/Form2.cs
+++ PTTK-GIaoDien/Form2.cs
@@ -20,22 +20,59 @@ namespace PTTK_GIaoDien
         private void guna2Button1_Click(object sender, EventArgs e)
         {
 
-            if (guna2TextBox1.Text == "" || guna2TextBox2.Text == "" || guna2TextBox3.Text == "" || guna2TextBox4.Text == "" || guna2TextBox5.Text == "" || guna2TextBox6.Text == "" || guna2TextBox7.Text == "" || guna2DateTimePicker1.Text.Length == 0)
+            if (guna2TextBox1.Text == "" || guna2TextBox2.Text == "" || guna2TextBox3.Text == "" || guna2TextBox4.Text == "" || guna2TextBox6.Text == "" || guna2TextBox7.Text == "" || guna2DateTimePicker1.Text.Length == 0)
             {
                 MessageBox.Show("Xin vui lòng điền đầy đủ thông tin");
             }
             else
             {
                 Connection.Connect();
-                string sql = "insert into KhachHang(CMND, DiaChiKH, EmailKH, HoTenKH, NgaySinhKH, SdtKH, SoFAX) values('" + guna2TextBox7.Text + "', '" + guna2TextBox4.Text + "', '" + guna2TextBox3.Text + "', '" + guna2TextBox1.Text + "', CONVERT(DATE,'" + guna2DateTimePicker1.Text + "' ,105)" + ", " + guna2TextBox2.Text + ", " + guna2TextBox6.Text + ");";
+                //string sql = "insert into KhachHang(CMND, DiaChiKH, EmailKH, HoTenKH, NgaySinhKH, SdtKH, SoFAX) values('" + guna2TextBox7.Text + "', '" + guna2TextBox4.Text + "', '" + guna2TextBox3.Text + "', '" + guna2TextBox1.Text + "', CONVERT(DATE,'" + guna2DateTimePicker1.Text + "' ,105)" + ", " + guna2TextBox2.Text + ", " + guna2TextBox6.Text + ");";
                 try
                     {
+                    //sql = "IF EXISTS(SELect 1 FROM Doan WHERE MaDoan = '" + 1 + "' SELECT 'MaDoan exists.'ELSE SELECT 'MaDoan does not exist.'; ";
+                    string sql = "select * from Doan where MaDoan = '"+ guna2TextBox5.Text +"';";
                     Connection.RunSQL(sql);
-                    MessageBox.Show("Them du lieu thanh cong");
+                    if(guna2TextBox5.Text == "")
+                    {
+                        try
+                        {
+                            sql = "insert into KhachHang(CMND, DiaChiKH, EmailKH, HoTenKH, NgaySinhKH, SdtKH, SoFAX) values('" + guna2TextBox7.Text + "', '" + guna2TextBox4.Text + "', '" + guna2TextBox3.Text + "', '" + guna2TextBox1.Text + "', CONVERT(DATE,'" + guna2DateTimePicker1.Text + "' ,105)" + ", " + guna2TextBox2.Text + ", " + guna2TextBox6.Text + ");";
+                            Connection.RunSQL(sql);
+                            MessageBox.Show("Them du lieu thanh cong");
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Them du lieu khong thanh cong");
+                        }
+                    }    
+                    else
+                    {
+                        if (Connection.GetFieldValues(sql) == "")
+                        {
+                            MessageBox.Show("Ma Doan khong ton tai");
+                        }
+                        else
+                        {
+
+                            try
+                            {
+                                sql = "insert into KhachHang(CMND, DiaChiKH, EmailKH, HoTenKH, NgaySinhKH, SdtKH, SoFAX) values('" + guna2TextBox7.Text + "', '" + guna2TextBox4.Text + "', '" + guna2TextBox3.Text + "', '" + guna2TextBox1.Text + "', CONVERT(DATE,'" + guna2DateTimePicker1.Text + "' ,105)" + ", " + guna2TextBox2.Text + ", " + guna2TextBox6.Text + ", " + guna2TextBox5.Text + ");";
+                                Connection.RunSQL(sql);
+                                MessageBox.Show("Them du lieu thanh cong");
+                            }
+                            catch
+                            {
+                                MessageBox.Show("Them du lieu khong thanh cong");
+                            }
+                        }
+                    }
                 }
+                    
                 catch
                 {
-                    MessageBox.Show(sql);
+
+                   
 
                 }
                 }           
