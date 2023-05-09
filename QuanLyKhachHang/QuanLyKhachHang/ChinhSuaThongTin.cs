c75fe442f81848dcac4831cfa64b02f5a983787b
ChLinh <93479230+ChLinh@users.noreply.github.com>
2023-05-09 18:23:00 +0700

    Add files via upload

---
+++ QuanLyKhachHang/QuanLyKhachHang/ChinhSuaThongTin.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1;
+namespace QuanLyKhachHang
+{
+    public partial class ChinhSuaThongTin : Form
+    {
+        public string Doan = "null";
+        public ChinhSuaThongTin()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Connection.Connect();
+            string CMND = "Select CMND from KhachHang where CMND = '" + textBox2.Text + "'";
+            string bufferCMND = Connection.GetFieldValues(CMND).Trim();
+            if (bufferCMND != "")
+            {
+                MessageBox.Show("Mã CMND đã tồn tại !!!");
+            }
+            else
+            {
+                string sql1 = "Select CMND from KhachHang where CMND = '" + textBox9.Text + "'";
+                string sql2 = "Select HoTenKH from KhachHang where CMND = '" + textBox9.Text + "'";
+                string sql3 = "Select DiaChiKH from KhachHang where CMND = '" + textBox9.Text + "'";
+                string sql4 = "Select EmailKH from KhachHang where CMND = '" + textBox9.Text + "'";
+                string sql5 = "Select NgaySinhKH from KhachHang where CMND = '" + textBox9.Text + "'";
+                string sql7 = "Select SdtKH from KhachHang where CMND = '" + textBox9.Text + "'";
+                string sql6 = "Select SoFAX from KhachHang where CMND = '" + textBox9.Text + "'";
+                string sql8 = "Select MaDoan from KhachHang where CMND = '" + textBox9.Text + "'";
+                textBox1.Text = Connection.GetFieldValues(sql1).Trim();
+                textBox2.Text = Connection.GetFieldValues(sql2).Trim();
+                textBox3.Text = Connection.GetFieldValues(sql3).Trim();
+                textBox4.Text = Connection.GetFieldValues(sql4).Trim();
+                textBox5.Text = Connection.GetFieldValues(sql5).Trim();
+                textBox6.Text = Connection.GetFieldValues(sql6).Trim();
+                textBox7.Text = Connection.GetFieldValues(sql7).Trim();
+                textBox8.Text = Connection.GetFieldValues(sql8).Trim();
+                Doan = Connection.GetFieldValues(sql8).Trim();
+                Connection.Disconnect();
+            }
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Connection.Connect();
+            MessageBox.Show(Doan);
+            string MaDoan = "Select MaDoan from Doan where MaDoan = '" + textBox8.Text + "'";
+            string bufferDoan = Connection.GetFieldValues(MaDoan).Trim();
+            if (textBox1.Text != textBox9.Text)
+            {
+                MessageBox.Show("Không được thay đổi CMND");
+                textBox1.Text = textBox9.Text;
+            }
+            else if (bufferDoan == "" && textBox8.Text != "")
+            {
+                MessageBox.Show("Mã Đoàn không tồn tại !!!");
+                textBox8.Text = Doan;
+            }
+            else
+            {
+                string sql = "update KhachHang set HoTenKH='" + textBox2.Text + "', DiaChiKH='" + textBox3.Text + "', EmailKH='" + textBox4.Text + "', NgaySinhKH= '" + textBox5.Text + "', SdtKH='" + textBox7.Text + "',SoFAX= '" + textBox6.Text + "', MaDoan='" + textBox8.Text + "' where CMND = '" + textBox9.Text + "'";
+                Connection.RunSQL(sql);
+            }
+        }
+    }
+}
---
+++ QuanLyKhachHang/QuanLyKhachHang/QuanLyKhachHang.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1;
+
+namespace QuanLyKhachHang
+{
+    public partial class QuanLyKhachHang : Form
+    {
+        public QuanLyKhachHang()
+        {
+            InitializeComponent();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void HienThiDSKH()
+        {
+            dataGridView1.ClearSelection();
+            Connection.Connect();
+            string sql = "Select CMND,HoTenKH,DiaChiKH,EmailKH,NgaySinhKH,SdtKH,SoFAX,MaDoan from KhachHang";
+            dataGridView2.DataSource = Connection.GetDataToTable(sql);
+
+            dataGridView2.EditMode = DataGridViewEditMode.EditProgrammatically;
+            Connection.Disconnect();
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            HienThiDSKH();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Connection.Connect();
+            string sql = "Select CMND,HoTenKH,DiaChiKH,EmailKH,NgaySinhKH,SdtKH,SoFAX,MaDoan from KhachHang where CMND = '" + textBox1.Text + "'";
+            dataGridView2.DataSource = Connection.GetDataToTable(sql);
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Connection.Connect();
+            string Doan = "null";
+
+            string MaDoan = "Select MaDoan from Doan where MaDoan = '" + textBox10.Text + "'";
+            string bufferDoan = Connection.GetFieldValues(MaDoan).Trim();
+            string CMND = "Select CMND from KhachHang where CMND = '" + textBox2.Text + "'";
+            string bufferCMND = Connection.GetFieldValues(CMND).Trim();
+            if (textBox2.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập CMND");
+            }
+            else if (textBox2.Text.Length != 10)
+            {
+                MessageBox.Show("Mã CMND không hợp lệ !!!");
+            }
+            else if (bufferCMND != "")
+            {
+                MessageBox.Show("Mã CMND đã tồn tại !!!");
+            }
+            else if (bufferDoan == "" && textBox10.Text!="")
+            {
+                MessageBox.Show("Mã Đoàn không tồn tại !!!");
+            }
+            else
+            {
+                Doan = bufferDoan;
+                if (Doan=="")
+                {
+                    string sql = "insert into KhachHang(CMND,HoTenKH,DiaChiKH,EmailKH,NgaySinhKH,SdtKH,SoFAX) values('" + textBox2.Text + "', '" + textBox4.Text + "', '" + textBox5.Text + "', '" + textBox6.Text + "', '" + textBox7.Text + "', '" + textBox8.Text + "', '" + textBox9.Text + "')";
+                    Connection.RunSQL(sql);
+                    MessageBox.Show("Thêm khách hàng thành công");
+                }
+                else
+                {
+                    string sql = "insert into KhachHang values('" + textBox2.Text + "', '" + textBox4.Text + "', '" + textBox5.Text + "', '" + textBox6.Text + "', '" + textBox7.Text + "', '" + textBox8.Text + "', '" + textBox9.Text + "','" + Doan + "')";
+                    Connection.RunSQL(sql);
+                    MessageBox.Show("Thêm khách hàng thành công");
+                }
+            }
+        }
+
+        private object Length(string buffer)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void NgaySinh_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ChinhSuaThongTin cs = new ChinhSuaThongTin();
+            cs.Show();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
