b87044eab5699f3631f323439c396c0ba30d787a
ChLinh <93479230+ChLinh@users.noreply.github.com>
2023-05-11 02:55:40 +0700

    Add files via upload

---
+++ QuanLyKhachSan/QuanLyKhachSan/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKhachSan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string secondAppPath = "D:/1PTTK/hotelManager-main/GiaoDienKhachHang/PTTK/obj/Debug/net6.0-windows/apphost.exe";
+            Process.Start(secondAppPath);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string secondAppPath = "D:/1PTTK/QuanLyKhachSan/GiaoDienNhanVien/WindowsFormsApp1/bin/Debug/WindowsFormsApp1.exe";
+            Process.Start(secondAppPath);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
